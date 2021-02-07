c44931beb3b997914952bf69e9bfbe777095d833
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-02-06 21:15:37 -0500

    adjust backend

--- client/src/components/fintech.vue
+++ client/src/components/fintech.vue
@@ -1,56 +1,111 @@
 <template>
-  <div class="whole">
-    <el-button type="primary" round @click="get">get</el-button>
-    <pre v-text="algorithm" class="codes"></pre>
-  </div>
+	<section class="whole">
+		<!-- libs -->
+		<el-card class="w100 scroll-h">
+			<div slot="header">
+				<span>1.Libs</span>
+			</div>
+			<div v-for="o in libs" :key="o">
+				{{ o }}
+			</div>
+		</el-card>
+		<!-- data -->
+		<el-card class="w100 scroll-h">
+			<div slot="header">
+				<span>2.Stock Data</span>
+			</div>
+			<div v-for="(v, i) in stockData" :key="i">{{ i }}:{{ v }}</div>
+		</el-card>
+		<!-- bootstrap -->
+		<el-card class="w100 scroll-h">
+			<div slot="header">
+				<span>3.Bootstrap: confidence interval(5%-95%) of mu</span>
+			</div>
+			<div v-for="(v, i) in bootstrapMU" :key="i">{{ i }}:{{ v }}</div>
+		</el-card>
+	</section>
 </template>
 
 <script>
 const axios = require("axios");
 export default {
-  name: "Fintech",
-  props: {
-    codeNo: String,
-  },
-  data: function () {
-    return {
-      codeNo2: "",
-      algorithm: "",
-    };
-  },
-  mounted() {},
-  methods: {
-    get: function () {
-      axios.get(`http://127.0.0.1:5000/api/codes/?pkg=${this.codeNo2}`).then(
-        (response) => {
-          if (response.data.error == "error") {
-            console.log("bakend error");
-          } else {
-            this.algorithm = response.data.result;
-          }
-        },
-        (err) => {
-          console.log("frontend error", err);
-        }
-      );
-    },
-  },
-  // 动态props
-  watch: {
-    codeNo: function (newVal) {
-      this.codeNo2 = newVal; //newVal即是codeNo
-    },
-  },
+	name: "Fintech",
+	data() {
+		return {
+			libs: [
+				"numpy",
+				"pandas",
+				"math",
+				"random",
+				"matplotlib",
+				"scipy",
+				"pandas_datareader",
+				"datetime",
+				"seaborn",
+			],
+			stockData: {
+				Open: [1, 2, 3],
+				Close: [1, 2, 3],
+				High: [1, 2, 3],
+				Low: [1, 2, 3],
+				Volume: [1, 2, 3],
+				"Adj Close": [1, 2, 3],
+			},
+			bootstrapMU: {
+				min: 4,
+				max: 5,
+			},
+		};
+	},
+	mounted() {},
+	methods: {
+		getStockData() {
+			axios.get(`http://127.0.0.1:5000/api/stockdata`).then(
+				(response) => {
+					if (response.data.error == "error") {
+						console.log("bakend error");
+					} else {
+						this.algorithm = response.data.result;
+					}
+				},
+				(err) => {
+					console.log("frontend error", err);
+				}
+			);
+		},
+		getBootStrapRst() {
+			axios.get(`http://127.0.0.1:5000/api/bootstrap`).then(
+				(response) => {
+					if (response.data.error == "error") {
+						console.log("bakend error");
+					} else {
+						this.algorithm = response.data.result;
+					}
+				},
+				(err) => {
+					console.log("frontend error", err);
+				}
+			);
+		},
+	},
 };
 </script>
 
 <style scoped>
 .whole {
-  overflow: hidden;
-  height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	height: 100%;
+	overflow-y: scroll;
 }
-.codes {
-  overflow: scroll;
-  height: 100vh;
+
+.w100 {
+	width: 100%;
+}
+
+.scroll-h {
+	overflow-x: scroll;
 }
 </style>
