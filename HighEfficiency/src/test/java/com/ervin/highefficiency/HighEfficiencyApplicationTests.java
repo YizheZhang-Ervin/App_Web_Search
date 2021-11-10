0520c5a9d07689b500fd0aec3b5bf430aace32e8
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-11-10 21:21:00 +0800

    add websocket

---
+++ HighEfficiency/src/test/java/com/ervin/highefficiency/HighEfficiencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ervin.highefficiency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighEfficiencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- practice-vue/src/Router/router.js
+++
@@ -1,8 +0,0 @@
-import Test from '../components/Test.vue'
-
-const routes = [
-  { path: '/', component: Test },
-  { path: '/test', component: Test }
-]
-
-export default routes;
